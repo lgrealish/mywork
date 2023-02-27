5aeb7fdea8adced056e2c529570535db9579a882
lgrealish <123647267+lgrealish@users.noreply.github.com>
2023-02-27 19:29:51 +0000

    week03

---
+++ week03/randomfruit.py
@@ -0,0 +1,12 @@
+# randomfruit
+# Author: Linda Grealish
+# This program prints out a random fruit
+# week 3 lab 1
+
+import random
+fruits = ['Apple', 'Orange', 'Banana', 'Pear']
+
+# we want a random number between 0 and lenght-1
+index = random.randint(0,len(fruits)-1)
+fruit = fruits[index]
+print("A Random Fruit:{}".format(fruit))
