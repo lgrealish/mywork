5f3f4ae58170b8cec7745b18814feb4f2f340835
lgrealish <123647267+lgrealish@users.noreply.github.com>
2023-02-28 15:49:14 +0000

    week03

--- week03/3.2.convert.py
+++ week03/3.2.convert.py
@@ -5,8 +5,11 @@
 # week 3 lab 2
 
 number = float(input("Please enter an amount:"))
+
 absolutevalue = abs(number)
-string2 = "The amount in cent is : "+str(absolutevalue * 100)
+
+# the code below was returning a .0 at the end of the result until I added the str() function
+string2 = "The amount in cent is : "+ str(int(absolutevalue * 100))
 
 print (string2)
 
