f2790c5aac1c52f29aeeb9c3a46ffb0a3284f285
lgrealish <123647267+lgrealish@users.noreply.github.com>
2023-03-06 14:38:12 +0000

    mywork

---
+++ week04/isEven.py
@@ -0,0 +1,13 @@
+# isEven.py
+# Author: Linda Grealish
+# This program takes in a number and 
+# tells the user if the number is odd or even
+# week 4 lab 1
+
+number = int (input("Enter an integer: "))
+
+if (number / 2) ==0:
+    print (f"{number} is an even number")
+
+else:
+    print (f"{number} is an odd number")
