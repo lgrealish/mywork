eeaf36cd278ff56c792012743d73ec67c5a98bb9
lgrealish <123647267+lgrealish@users.noreply.github.com>
2023-03-16 20:26:46 +0000

    mywork

---
+++ week05/months.py
@@ -0,0 +1,18 @@
+months = ("January", 
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December")
+
+summer = months[4:7]
+
+for month in summer:
+    print (month)
+
+
