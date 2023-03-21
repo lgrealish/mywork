e2d1268cef31cf830bc1a27b28220d8587ac2484
lgrealish <123647267+lgrealish@users.noreply.github.com>
2023-03-21 20:53:08 +0000

    mywork

---
+++ week06/week06-functions.py
@@ -0,0 +1,56 @@
+# week06-functions.py.py
+# Author: Linda Grealish
+# Program that allows a user to create new students and to view students
+# week 06 lab
+
+def displayMenu():
+ print("What would you like to do?")
+ print("\t(a) Add new student")
+ print("\t(v) View students")
+ print("\t(q) Quit")
+ choice = input("Type one letter (a/v/q):").strip()
+ return choice
+
+def doAdd(students):
+ currentStudent = {}
+ currentStudent["name"]=input("enter name :")
+ currentStudent["modules"]= readModules()
+ students.append(currentStudent)
+
+def readModules():
+    modules = []
+    moduleName = input("\Enter the first Module name (blank to quit) :").strip()
+
+    while moduleName != "":
+            module = {}
+            module["name"]= moduleName
+            # I am not doing error handling
+            module["grade"]=int(input("\t\tEnter grade:"))
+            modules.append(module)
+            # now read the next module name
+            moduleName = input("\tEnter next module name (blank to quit) :").strip()
+
+    return modules
+
+def displayModules(modules):
+    print ("\tName \tGrade")
+    for module in modules:
+        print(f"\t{ module['name']} \t{ module['grade']}") 
+
+def doView(students):
+    for currentStudent in students:
+        print(currentStudent["name"])
+        displayModules(currentStudent["modules"]);
+
+
+students =[]
+choice = displayMenu()
+while (choice != 'q'):
+    if choice == 'a':
+        doAdd(students)
+    elif choice == 'v':
+        doView(students)
+    elif choice !='q':
+        print("\n\nplease select either a,v or q")
+
+    choice = displayMenu()
